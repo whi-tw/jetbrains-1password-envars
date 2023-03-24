3a11a776651857254753cd987aa22be0e245cd40
GitHub Action <action@github.com>
2023-03-24 10:33:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/whitw/jetbrains1passwordenvars/MyBundle.kt
+++ src/main/kotlin/com/github/whitw/jetbrains1passwordenvars/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.whitw.jetbrains1passwordenvars
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/whitw/jetbrains1passwordenvars/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/whitw/jetbrains1passwordenvars/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.whitw.jetbrains1passwordenvars.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
